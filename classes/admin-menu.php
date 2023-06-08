9bca608acd2af0afb07adcf50d442f056a0927b2
Addisu <addisuhaile87@gmail.com>
2023-06-08 11:09:12 +0300

    Create admin-menu and plugin

---
+++ classes/admin-menu.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+This file will create admin menu page
+*/
+
+class WPDE_Create_Admin_Page {
+  public function __construct() {
+    add_action("admin_menu", [$this, "create_admin_menu"]);
+  }
+
+  public function create_admin_menu() {
+    $capablity = 'manage_options';
+    $slug = "wprk-settings";
+
+
+    add_menu_page(
+      __("WP Dowell Email", "wp-dowell-email"),
+      __("WP Dowell Email", "wp-dowell-email"),
+      $capablity,
+      $slug,
+      [$this, "menu_page_template"],
+      "dashicons-buddicons-replies",
+
+    );
+  }
+
+  public function menu_page_template() {
+    echo '<div class="wrap"><div id="wpde-admin-app"></div></div>';
+  }
+}
+
+new WPDE_Create_Admin_Page();
--- dowell-email-plugin.php
+++ dowell-email-plugin.php
@@ -1,8 +1,29 @@
 <?php
-/**
+/*
  * Plugin Name: doWell-email-plugin
  * Description: A doWell email plugin
  * Version: 1.0
  * Author: Addisu Haile
  * Author URI: https://github.com/Addisu87
- **/
+ */
+
+ if(!defined("ABSPATH")) : exit(); endif; // No direct access allowed.
+
+/** Define plugins Constants */
+define ( 'WPDE_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define ( 'WPDE_URL', trailingslashit( plugins_url( '/', __FILE__ ) ) );
+
+
+/** Loading necessary scripts */
+
+add_action("admin_enqueue_scripts", "load_scripts");
+
+function load_scripts() {
+  wp_enqueue_scripts("wp-dowell-email", WPDE_URL . "dist/bundle.js", ["wp-element"], wp_rand(), true);
+  wp_localize_script("wp-dowell-email", "appLocalizer", [
+    'apiUrl' => home_url('/wp-json'),
+    "nonce" => wp_create_nonce("wp-rest")
+  ]);
+}
+
+require_once WPDE_PATH . "classes/admin-menu.php";
