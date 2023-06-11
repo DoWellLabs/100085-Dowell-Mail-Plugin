d778f0bed0c71517c3ada277869361a5736ee3fe
Addisu <addisuhaile87@gmail.com>
2023-06-11 21:37:04 +0300

    Update the recat dom rendering method

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var element = document.getElementById("wp-dowell-email");
-//   if (typeof element !== "undefined" && element !== null) {
-//     ReactDOM.render(<App />, document.getElementById("wp-dowell-email"));
-//   }
-// });
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
